4d600c057904372ca34b4246ad2aa6292d03997b
Imisrael <imru@fixstars.com>
2022-10-05 16:12:54 -0700

    Update ingest.R

--- ingest.R
+++ ingest.R
@@ -10,6 +10,7 @@ drv <- JDBC("com.toshiba.mwcloud.gs.sql.Driver",
             identifier.quote = "`")
 
 conn <- dbConnect(drv, "jdbc:gs://127.0.0.1:20001/myCluster/public", "admin", "admin")
+print("Succesfully connected to GridDB")
 
 library(nflreadr)
 loader <- rds_from_url
@@ -22,6 +23,10 @@ out <- rbindlist_with_attrs(out)
 
 out$type_abbreviation <- NULL
 
+print("Beginning ingest. Please hang tight as this process may take a few hours+")
+
 for (i in 1:nrow(out)) {
   RJDBC::dbWriteTable(conn, "nba_pbp_2022", out[i, ], append = TRUE )
 }
+
+print("Thank you for your patience. The ingest has completed.")
